ed4440943f66dfb6de131619aaaac2845fe45ce4
Arnon.r <71333954+richmanz08@users.noreply.github.com>
2025-01-23 09:33:51 +0700

    get start to do home and navbar

---
+++ src/components/home/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeComponent: React.FC<HomeComponentProps> = (props) => {
+  const {} = props;
+  return (
+    <div className="bg-red-400 flex h-6 w- text-fuchsia-600">
+      Home hellowolrd
+    </div>
+  );
+};
+
+export default HomeComponent;
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavbarComponent: React.FC<NavbarComponentProps> = (props) => {
+  const {} = props;
+  return (
+    <div className="bg-red-400 w-full flex h-10 text-cyan-700 text-fuchsia-600">
+      Navbar hellowolrd
+    </div>
+  );
+};
+
+export default NavbarComponent;
